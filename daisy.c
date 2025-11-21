1cbc5e0b8dccc4926045f31fedd4126eb527d2d6
Arisiima Daisy <arisiimadaisy@gmail.com>
2025-11-21 15:14:58 +0300

    hello Daisy

---
+++ daisy.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char str[];
+    int strlength;
+    printf("enter the string");
+    fgets(str,sizeof(str),stdin);
+    strlength=strlen
+    printf("the length is:");
+    puts(str);
+    return 0;
+}
